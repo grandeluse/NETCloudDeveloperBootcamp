6bec3fef77629bc7ec1a6b9bb9e082847f9c188c
Francesco Ledda <francesco.ledda@swisscom.com>
2024-11-30 10:36:58 +0100

    feat: NETCoreEssential - Expanding the data model

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -5,31 +5,43 @@ var app = builder.Build();
 
 const string GetGameEndpointName = "GetGame";
 
+List<Genre> genres =
+[
+    new(){Id = new Guid("c78cc485-4174-48cd-afa3-06c623075867"), Description = "Fighting"},
+    new(){Id = new Guid("fc28eda1-cc34-4d6e-b300-02e52867a492"), Description = "Kids and Family"},
+    new(){Id = new Guid("b2fc74da-10bb-4fb2-af32-f21a7f9f071f"), Description = "Racing"},
+    new(){Id = new Guid("be68c3d5-5cef-4eb0-ab3e-b3a913a77d9b"), Description = "Roleplaying"},
+    new(){Id = new Guid("e759edd3-f83d-4c8e-b110-993e30b1edc5"), Description = "Sports"}
+];
+
 List<Game> games =
 [
     new()
     {
         Id = Guid.NewGuid(),
         Name = "Street Fighter II",
-        Genre = "Fighting",
+        Genre = genres[0],
         Price = 19.99m,
-        ReleaseDate = new DateOnly(1992, 7, 15)
+        ReleaseDate = new DateOnly(1992, 7, 15),
+        Description = "Street Fighter II: The World Warrior is a 1991 fighting game produced by Capcom for arcades, and their fourteenth game to use the CP System arcade system board"
     },
     new()
     {
         Id = Guid.NewGuid(),
         Name = "Final Fantasy XIV",
-        Genre = "Roleplaying",
+        Genre = genres[3],
         Price = 59.99m,
-        ReleaseDate = new DateOnly(2010, 9, 30)
+        ReleaseDate = new DateOnly(2010, 9, 30),
+        Description = "Final Fantasy XIV is a massively multiplayer online role-playing game (MMORPG) developed and published by Square Enix"
     },
     new()
     {
         Id = Guid.NewGuid(),
         Name = "FIFA 23",
-        Genre = "Sports",
+        Genre = genres[4],
         Price = 69.99m,
-        ReleaseDate = new DateOnly(2022, 9, 27)
+        ReleaseDate = new DateOnly(2022, 9, 27),
+        Description = "FIFA 23 is a football video game published by EA Sports. It is the 30th and final installment in the FIFA series that is developed by EA Sports"
     }
 ];
 
