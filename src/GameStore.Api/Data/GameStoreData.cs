4ad8fcfb8c67f67760503c29e142652ced969d86
Francesco Ledda <francesco.ledda@swisscom.com>
2024-11-30 22:10:59 +0100

    feat: NETCoreEssential - rename private field

--- src/GameStore.Api/Data/GameStoreData.cs
+++ src/GameStore.Api/Data/GameStoreData.cs
@@ -4,7 +4,7 @@ namespace GameStore.Api.Data;
 
 public class GameStoreData
 {
-    private readonly List<Genre> genres =
+    private readonly List<Genre> _genres =
     [
         new(){Id = new Guid("c78cc485-4174-48cd-afa3-06c623075867"), Name = "Fighting"},
         new(){Id = new Guid("fc28eda1-cc34-4d6e-b300-02e52867a492"), Name = "Kids and Family"},
@@ -13,17 +13,17 @@ public class GameStoreData
         new(){Id = new Guid("e759edd3-f83d-4c8e-b110-993e30b1edc5"), Name = "Sports"}
     ];
 
-    private readonly List<Game> games;
+    private readonly List<Game> _games;
 
     public GameStoreData()
     {
-        games =
+        _games =
         [
             new Game
             {
                 Id = Guid.NewGuid(),
                 Name = "Street Fighter II",
-                Genre = genres[0],
+                Genre = _genres[0],
                 Price = 19.99m,
                 ReleaseDate = new DateOnly(1992, 7, 15),
                 Description = "Street Fighter II: The World Warrior is a 1991 fighting game produced by Capcom for arcades, and their fourteenth game to use the CP System arcade system board"
@@ -32,7 +32,7 @@ public class GameStoreData
             {
                 Id = Guid.NewGuid(),
                 Name = "Final Fantasy XIV",
-                Genre = genres[3],
+                Genre = _genres[3],
                 Price = 59.99m,
                 ReleaseDate = new DateOnly(2010, 9, 30),
                 Description = "Final Fantasy XIV is a massively multiplayer online role-playing game (MMORPG) developed and published by Square Enix"
@@ -41,7 +41,7 @@ public class GameStoreData
             {
                 Id = Guid.NewGuid(),
                 Name = "FIFA 23",
-                Genre = genres[4],
+                Genre = _genres[4],
                 Price = 69.99m,
                 ReleaseDate = new DateOnly(2022, 9, 27),
                 Description = "FIFA 23 is a football video game published by EA Sports. It is the 30th and final installment in the FIFA series that is developed by EA Sports"
@@ -49,22 +49,22 @@ public class GameStoreData
         ];
     }
 
-    public IEnumerable<Game> GetGames() => games;
+    public IEnumerable<Game> GetGames() => _games;
     
-    public Game? GetGame(Guid id) => games.Find(game => game.Id == id);
+    public Game? GetGame(Guid id) => _games.Find(game => game.Id == id);
 
     public void AddGame(Game game)
     {
         game.Id = Guid.NewGuid();
-        games.Add(game);
+        _games.Add(game);
     }
 
     public void RemoveGame(Guid id)
     {
-        games.RemoveAll(game => game.Id == id);
+        _games.RemoveAll(game => game.Id == id);
     }
     
-    public IEnumerable<Genre> GetGenres() => genres;
+    public IEnumerable<Genre> GetGenres() => _genres;
 
-    public Genre? GetGenre(Guid id) => genres.Find(genre => genre.Id == id);
+    public Genre? GetGenre(Guid id) => _genres.Find(genre => genre.Id == id);
 }
