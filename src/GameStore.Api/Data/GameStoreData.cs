c8fdfb12befd58188ed3e2840c94367d95cc8baa
Francesco Ledda <francesco.ledda@swisscom.com>
2024-12-18 09:12:51 +0100

    feat: NETCoreEssential - Entity Framework and Configuration System - Preparing the data model for EF Core

--- src/GameStore.Api/Data/GameStoreData.cs
+++ src/GameStore.Api/Data/GameStoreData.cs
@@ -24,6 +24,7 @@ public class GameStoreData
                 Id = Guid.NewGuid(),
                 Name = "Street Fighter II",
                 Genre = _genres[0],
+                GenreId = _genres[0].Id,
                 Price = 19.99m,
                 ReleaseDate = new DateOnly(1992, 7, 15),
                 Description = "Street Fighter II: The World Warrior is a 1991 fighting game produced by Capcom for arcades, and their fourteenth game to use the CP System arcade system board"
@@ -33,6 +34,7 @@ public class GameStoreData
                 Id = Guid.NewGuid(),
                 Name = "Final Fantasy XIV",
                 Genre = _genres[3],
+                GenreId = _genres[3].Id,
                 Price = 59.99m,
                 ReleaseDate = new DateOnly(2010, 9, 30),
                 Description = "Final Fantasy XIV is a massively multiplayer online role-playing game (MMORPG) developed and published by Square Enix"
@@ -42,6 +44,7 @@ public class GameStoreData
                 Id = Guid.NewGuid(),
                 Name = "FIFA 23",
                 Genre = _genres[4],
+                GenreId = _genres[4].Id,
                 Price = 69.99m,
                 ReleaseDate = new DateOnly(2022, 9, 27),
                 Description = "FIFA 23 is a football video game published by EA Sports. It is the 30th and final installment in the FIFA series that is developed by EA Sports"
