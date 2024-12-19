03bd83647a453b710c3527e629f384d9640c578e
Francesco Ledda <francesco.ledda@swisscom.com>
2024-12-19 19:17:26 +0100

    feat: NETCoreEssential - Entity Framework and Configuration System - Using ASP.NET Core configuration

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -4,21 +4,7 @@ using GameStore.Api.Features.Genres;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "Data Source=GameStore.db";
-
-/* What service lifetime to use for a dbContext?
-- DbContext is designed to used as single Unit of Work
-- DbContext created --> entity changes are tracked -> save changes -> dispose
-- DB connections are expensive
-- DbContext is not thread safe
-- Increased memory usage due to change tracking
- 
- USE: Scoped service lifetime
-- Aligning the context lifetime to the lifetime of the request
-- There is only one thread executing each client request at a given time 
-- Ensure that each request gets a separate DbContext instance
-*/
-
+var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 builder.Services.AddTransient<GameDataLogger>();
