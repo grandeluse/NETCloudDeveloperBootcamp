48c1a76dd7590680290b29600a68283c93e3ebb2
francesco.ledda <francesco.ledda@adesso.ch>
2025-01-02 22:27:34 +0100

    feat: NETCoreEssential - Working with Data - Final queries and clean up

--- src/GameStore.Api/Features/Genres/GetGenres/GetGenresEndpoint.cs
+++ src/GameStore.Api/Features/Genres/GetGenres/GetGenresEndpoint.cs
@@ -1,4 +1,5 @@
 using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Genres.GetGenres;
 
@@ -7,7 +8,9 @@ public static class GetGenresEndpoint
     public static void MapGetGenres(this IEndpointRouteBuilder app)
     {
         // GET /genres
-        app.MapGet("/", (GameStoreData data) => 
-            data.GetGenres().Select(genre => new GenreDto(genre.Id, genre.Name)));
+        app.MapGet("/", (GameStoreContext dbContext) => 
+            dbContext.Genres
+                .Select(genre => new GenreDto(genre.Id, genre.Name))
+                .AsNoTracking());
     }
 }
