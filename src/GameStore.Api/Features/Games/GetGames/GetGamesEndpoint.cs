5152b812cff4d25742d619cd3e1a7fd9cd15e026
francesco.ledda <francesco.ledda@adesso.ch>
2025-01-02 22:13:14 +0100

    feat: NETCoreEssential - Working with Data - Querying all records from the database

--- src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -1,4 +1,5 @@
 using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Games.GetGames;
 
@@ -7,12 +8,15 @@ public static class GetGamesEndpoint
     public static void MapGetGames(this IEndpointRouteBuilder app)
     {
         // GET /games
-        app.MapGet("/", (GameStoreData data) => data.GetGames()
-            .Select(game => new GameSummaryDto(
-                game.Id, 
-                game.Name, 
-                game.Genre!.Name, 
-                game.Price, 
-                game.ReleaseDate)));
+        app.MapGet("/", (GameStoreContext context) => 
+            context.Games
+                .Include(game => game.Genre)
+                .Select(game => new GameSummaryDto(
+                    game.Id, 
+                    game.Name, 
+                    game.Genre!.Name, 
+                    game.Price, 
+                    game.ReleaseDate))
+                .AsNoTracking());
     }
 }
