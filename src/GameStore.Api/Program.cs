6193151c3243c705b31546116d34d65b58a90f56
Francesco Ledda <francesco.ledda@swisscom.com>
2025-01-07 20:50:30 +0100

    feat: ASP.NET Core Advanced - Middleware - Adding middleware via a request delegate

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using GameStore.Api.Data;
 using GameStore.Api.Features.Games;
 using GameStore.Api.Features.Genres;
@@ -11,6 +12,31 @@ var app = builder.Build();
 
 app.MapGames();
 app.MapGenres();
+
+app.Use(async(context, next) =>
+{
+    var stopwatch = new Stopwatch();
+
+    try
+    {
+        stopwatch.Start();
+
+        await next(context);
+    }
+    finally
+    {
+        stopwatch.Stop();
+        var elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+        app.Logger.LogInformation("{RequestMethod} {RequestPath} completed with {Status} in {ElapsedMilliseconds} ms.",
+            context.Request.Method,
+            context.Request.Path,
+            context.Response.StatusCode,
+            elapsedMilliseconds
+        );
+    }
+});
+
+
 
 await app.InitializeDbAsync();
 
