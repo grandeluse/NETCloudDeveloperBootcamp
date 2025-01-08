5bf9d471b312b65b79f5dced15a2e2aeae7e9d91
Francesco Ledda <francesco.ledda@swisscom.com>
2025-01-08 10:12:11 +0100

    feat: ASP.NET Core Advanced - Middleware - Use of HttpLogging Middleware

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -1,20 +1,31 @@
-using System.Diagnostics;
 using GameStore.Api.Data;
 using GameStore.Api.Features.Games;
 using GameStore.Api.Features.Genres;
 using GameStore.Api.Shared.Timing;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.RequestMethod |
+                            HttpLoggingFields.RequestPath |
+                            HttpLoggingFields.ResponseStatusCode |
+                            HttpLoggingFields.Duration;
+    options.CombineLogs = true;
+});
+
 var app = builder.Build();
 
 app.MapGames();
 app.MapGenres();
 
-app.UseMiddleware<RequestTimingMiddleware>();
+
+//app.UseMiddleware<RequestTimingMiddleware>();
+app.UseHttpLogging();
 
 await app.InitializeDbAsync();
 
