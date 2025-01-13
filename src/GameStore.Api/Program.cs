81b13239a72aa65f8a1e640b857ec3418a75c097
Francesco Ledda <francesco.ledda@swisscom.com>
2025-01-13 20:57:11 +0100

    feat: ASP.NET Core Advanced - Error Handling - Add problem details support

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -1,10 +1,11 @@
 using GameStore.Api.Data;
 using GameStore.Api.Features.Games;
 using GameStore.Api.Features.Genres;
-using GameStore.Api.Shared.Timing;
 using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
 
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
@@ -26,6 +27,13 @@ app.MapGenres();
 
 //app.UseMiddleware<RequestTimingMiddleware>();
 app.UseHttpLogging();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+
+app.UseStatusCodePages();
 
 await app.InitializeDbAsync();
 
