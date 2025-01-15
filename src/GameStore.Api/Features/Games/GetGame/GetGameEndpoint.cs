0f8404df6957b95b4b64a701b2397de9eaa0cbc4
Francesco Ledda <francesco.ledda@swisscom.com>
2025-01-15 08:47:32 +0100

    feat: ASP.NET Core Advanced - Error Handling - Catching and logging errors

--- src/GameStore.Api/Features/Games/GetGame/GetGameEndpoint.cs
+++ src/GameStore.Api/Features/Games/GetGame/GetGameEndpoint.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using GameStore.Api.Data;
 using GameStore.Api.Features.Games.Constants;
 using GameStore.Api.Models;
@@ -10,19 +11,42 @@ public static class GetGameEndpoint
     public static void MapGetGame(this IEndpointRouteBuilder app)
     {
         // GET /games/{id}
-        app.MapGet("/{id}", async (Guid id, GameStoreContext dbContext) =>
+        app.MapGet("/{id}", async (Guid id, GameStoreContext dbContext, ILogger<Program> logger) =>
         {
-            var game = await FindGameAsync(dbContext, id);
+            try
+            {
+                var game = await FindGameAsync(dbContext, id);
 
-            return game is null ? Results.NotFound() : Results.Ok(
-                new GameDetailsDto(
-                    game.Id, 
-                    game.Name, 
-                    game.GenreId, 
-                    game.Price, 
-                    game.ReleaseDate, 
-                    game.Description));
-        }).WithName(EndpointNames.GetGame);
+                return game is null
+                    ? Results.NotFound()
+                    : Results.Ok(
+                        new GameDetailsDto(
+                            game.Id,
+                            game.Name,
+                            game.GenreId,
+                            game.Price,
+                            game.ReleaseDate,
+                            game.Description));
+            }
+            catch (Exception ex)
+            {
+                var traceId = Activity.Current?.TraceId;
+                logger.LogError(
+                    ex,
+                    "could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    traceId);
+                return Results.Problem(
+                    title: "An error occurred while processing your request",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object?>
+                    {
+                        { "traceId", traceId.ToString() }
+                    }
+                );
+            }
+        })
+        .WithName(EndpointNames.GetGame);
     }
 
     private static async Task<Game?> FindGameAsync(GameStoreContext dbContext, Guid id)
